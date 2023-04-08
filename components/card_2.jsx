33f8693b975561a1e9f6d21d7500fa7aa4bffec0
ap2684 <ap2684@njit.edu>
2023-04-08 16:14:41 -0400

    homepage layout setup

---
+++ components/card_2.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const MyCard2 = () => {
+  return (
+    <Container className="my-5">
+      <Row>
+        <Col md={6}>
+          <div className="my-3">
+            <h3>Example Card</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat ante sit amet eros faucibus, vel euismod tortor varius. Integer id lectus vel felis sodales sollicitudin. Fusce faucibus urna ac velit interdum, non mattis arcu elementum.</p>
+            <p>Donec a luctus augue. Mauris id nisl vel ipsum ultricies bibendum eu quis elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur et ante vitae magna eleifend hendrerit at id sem.</p>
+          </div>
+        </Col>
+        <Col md={6}>
+          <img src="https://picsum.photos/400" alt="Example" className="img-fluid rounded" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default MyCard2;
