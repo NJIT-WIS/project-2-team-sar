ea29b218f60692d5217c9793b86b9cc5ba67c7b7
ap2684 <ap2684@njit.edu>
2023-04-16 22:48:36 -0400

    testing the playwright file bug fix4

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Head>
         <title>SAR Marketing Group</title>
-        <meta name="description" content="Marketing group site created by  three college students." />
+        <meta name="description" content="Marketing group site created by three college students." />
       </Head>
       <Header />
       <MyCard1 />
