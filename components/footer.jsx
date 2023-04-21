239739061501eaa5beff180e062a20735d1845aa
Swapnil62 <sp2428@njit.edu>
2023-04-20 22:47:03 -0400

    updated footer.jsx and about.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,21 +1,31 @@
+import Link from 'next/Link';
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-
 const Footer = () => {
   return (
-    <footer className="bg-light">
+    <footer className="bg-light py-1">
       <Container>
         <Row>
-          <Col md={6}>
+          <Col md={4}>
+            <ul class="nav">
+              <li class="nav-item">
+                <Link href="/aboutus" class="nav-link active" aria-current="page">About Us</Link>
+              </li>
+              <li class="list-inline-item">
+                <Link href="/privacy" class="nav-link active" aria-current="page">Privacy Policy</Link>
+              </li>
+            </ul>
+          </Col>
+          <Col md={4}>
             <h5>Connect with us:</h5>
             <a href="https://www.facebook.com"><i className="fab fa-facebook fa-2x"></i></a>
             <a href="https://www.twitter.com"><i className="fab fa-twitter fa-2x"></i></a>
             <a href="https://www.instagram.com"><i className="fab fa-instagram fa-2x"></i></a>
           </Col>
-          <Col md={6}>
+          <Col md={4}>
             <h5>Contact us:</h5>
             <p>123 Main Street, New York, NY 10001</p>
             <p>Email: info@example.com</p>
@@ -23,12 +33,13 @@ const Footer = () => {
           </Col>
         </Row>
         <Row>
-          <Col className="text-center mt-3">
+          <Col className="text-center mt-3 py-2">
             <p>&copy; {new Date().getFullYear()} Example Company, Inc. All rights reserved.</p>
           </Col>
         </Row>
       </Container>
     </footer>
+
   );
 };
 
