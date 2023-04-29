181696d3a636bb947fc77207154067c07ab278a6
rlantigua01 <123276441+rlantigua01@users.noreply.github.com>
2023-04-28 21:17:59 -0400

    mail chimp_integration.jsx

---
+++ components/mailchimp_integration.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+function EmailForm() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`https://us6.api.mailchimp.com/3.0/lists/${process.env.REACT_APP_MAILCHIMP_LIST_ID}/members`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${Buffer.from(`anystring:${process.env.REACT_APP_MAILCHIMP_API_KEY}`).toString('base64')}`
+      },
+      body: JSON.stringify({
+        email_address: email,
+        status: 'subscribed'
+      })
+    });
+
+    if (response.ok) {
+      setEmail('');
+      alert('Thanks for subscribing!');
+    } else {
+      alert('Sorry, something went wrong. Please try again later.');
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formEmail">
+        <Form.Label>Enter your email address to receive updates:</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+      </Form.Group>
+      <Button variant="primary" type="submit">Subscribe</Button>
+    </Form>
+  );
+}
+
+export default EmailForm;
