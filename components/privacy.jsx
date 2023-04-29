76071939ad4f5c2184d421ca76e0a877c7d7d331
Swapnil62 <sp2428@njit.edu>
2023-04-28 22:21:47 -0400

    Updated and added new policies

--- components/privacy.jsx
+++ components/privacy.jsx
@@ -5,23 +5,21 @@ const Privacy = () => {
     return ( 
 
         <Container className="my-5">
-            <Row class="col-lg-10 mx-auto">
+            <Row class="col-lg-10 mx-auto lead">
                 <h1 className="text-left mb-4">Privacy Policy</h1>
                 <p className="mt-2">Date Updated: April 22, 2023</p>
                 <hr></hr>
 
-                <div class="lead mt-2">
                     <p> MyWebClass ("us", "we", or "our") operates the mywebclass.org website (the "Site"). </p>
                     <p>Your privacy is important to us. It is MyWebClass' policy to respect your privacy regarding any information we may collect from you across our website, mywebclass.org, and other sites we own and operate.</p>
                     <p>This page informs you of our policies regarding the collection, use, and disclosure of Personal Information we receive from users of the Site.</p>
                     <p>We use your Personal Information only for providing and improving the Site. By using the Site, you agree to the collection and use of information in accordance with this policy.</p>
-                </div>
-                <div class="lead">
+                
                 <h3 id="Information-collected">Information we collect</h3>
                     <hr></hr>
                     <p>While using our Site, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. Personally identifiable information may include, but is not limited to your name, email address, phone number, and postal address ("Personal Information").</p>
                     <p> We may use your Personal Information to contact you with newsletters, marketing or promotional materials and other information that may be of interest to you. You may opt-out of receiving any, or all, of these communications from us by following the unsubscribe link or instructions provided in any email we send.</p>
-                </div>    
+               
                 <h3 id="Log data">Log Data</h3>
                     <hr></hr>
                     <p>Like many site operators, we collect information that your browser sends whenever you visit our Site ("Log Data").</p>
@@ -43,6 +41,54 @@ const Privacy = () => {
                     </ul>
                     <p>Please note that we may ask you to verify your identity before responding to such requests.</p>
                     <p>If you are a resident of the EEA, you have the right to complain to a Data Protection Authority about our collection and use of your Personal Data. For more information, please contact your local data protection authority in the EEA.</p>
+                
+                <h3 id="Google Analytics">Google Analytics</h3>
+                    <hr></hr>   
+                    <p>We use Google Analytics to analyze the use of our website. Google Analytics gathers information about website use by means of cookies. The information gathered relating to our website is used to create reports about the use of our website. Google's privacy policy is available at: https://policies.google.com/privacy.</p>
+            
+                <h3 id="Digital Millennium Copyright Act (DMCA)">Digital Millennium Copyright Act (DMCA)</h3>
+                    <hr></hr>
+                    <p> We respect the intellectual property rights of others and expect our users to do the same. If you believe that any content on our website infringes your copyright, please contact us immediately, with the following information:</p>
+                    <ul>
+                        <li> A description of the copyrighted work that you claim has been infringed;</li>
+                        <li> A description of where the material that you claim is infringing is located on our website;</li>
+                        <li> Your contact information, including your name, address, telephone number, and email address;</li>
+                        <li> A statement by you that you have a good faith belief that the disputed use is not authorized by the copyright owner, its agent, or the law;</li>
+                        <li> A statement by you, made under penalty of perjury, that the above information in your notice is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf.</li>
+                    </ul>
+
+                <h3 id="California Consumer Privacy Act (CCPA)">California Consumer Privacy Act (CCPA)</h3>
+                    <hr></hr>
+                    <p> Under the CCPA, California residents have certain data protection rights, including:</p>
+                    <ul>
+                        <li> The right to know what personal information we have collected about you and how it was used;</li>
+                        <li> The right to request that we delete your personal information;</li>
+                        <li> The right to opt-out of the sale of your personal information;</li>
+                        <li> The right to non-discrimination for exercising your CCPA rights.</li>
+                    </ul>
+                    <p> If you are a California resident and would like to exercise your CCPA rights, please contact us. We may ask you to verify your identity before responding to your request.</p>
+                    <p> Please note that we do not sell your personal information.</p>
+                <h3 id="Information sharing">Information sharing and disclosure</h3>
+                    <hr></hr>    
+                    <p>We may share your information with third-party service providers who assist us with our business operations, such as website hosting, payment processing, and data analysis. We may also share your information in response to a legal request, such as a subpoena or court order, or to protect our rights or the rights of others.</p>
+            
+                <h3 id="Cookies">Cookies</h3>
+                    <hr></hr>
+                    <p>Cookies are files with small amounts of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your computer's hard drive.</p>
+                    <p>Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Site.</p>
+
+                <h3 id="Security">Security</h3>
+                    <hr></hr>
+                    <p>We take reasonable measures to protect the personal information we collect from unauthorized access, use, and disclosure. However, no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee the absolute security of your information.</p>
+
+                <h3 id="Childrens Privacy">Children's privacy</h3>
+                    <hr></hr>
+                    <p>Our Site does not address anyone under the age of 18 ("Children"). We do not knowingly collect personally identifiable information from children under 18. If you are a parent or guardian and you are aware that your child has provided us with Personal Information, please contact us. If we become aware that we have collected Personal Information from children under 18 without verification of parental consent, we take steps to remove that information from our servers.</p>
+
+                <h3 id="Changes to policy">Changes to this policy</h3>
+                    <hr></hr>
+                    <p>We may update this privacy policy from time to time by posting a new version on our website. You should check this page occasionally to ensure you are comfortable with any changes.</p>
+                    
                 </Row>
         </Container>
 
