22637deb108b13754454f32af561fb69e754d228
rlantigua01 <123276441+rlantigua01@users.noreply.github.com>
2023-04-30 04:14:37 -0400

    Update subscribe.js

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import Resources from '../components/resources'
 import EmailForm from '../components/mailchimp_integration';
 
 function Subscribe() {
