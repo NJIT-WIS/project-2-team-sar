145a645d84aeaf1f6fa8ed95ad82b5982c70afa2
Swapnil62 <sp2428@njit.edu>
2023-04-29 23:13:47 -0400

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,26 @@
 //import '../styles/globals.css'
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import script from 'next/script'
+
+function App({ Component, pageProps }) {
+  return (  
+      <>
+        <script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}"/>
+        <script id="gtag-init" strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',{
+                page_path: window.location.pathname,
+              });
+              `,
+          }}
+          />     
+        <Component {...pageProps} />
+      
+      </>
+  )
 }
+
+export default App
