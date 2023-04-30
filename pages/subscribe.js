9a2212a75dc916050f28a1539a4861916ccf9756
rlantigua01 <123276441+rlantigua01@users.noreply.github.com>
2023-04-30 03:58:02 -0400

    Create subscribe.js

---
+++ pages/subscribe.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Resources from '../components/resources'
+import EmailForm from '../components/EmailForm';
+
+function Subscribe() {
+  return (
+    <Container>
+      <h1>Subscribe to Our Newsletter</h1>
+      <EmailForm />
+    </Container>
+  );
+}
+
+export default Subscribe;
