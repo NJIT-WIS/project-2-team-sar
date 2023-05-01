0d469f2bc60537ccfb2d4b9b18ead9c1215a1db6
rlantigua01 <123276441+rlantigua01@users.noreply.github.com>
2023-05-01 06:44:40 -0400

    Update subscribe.js

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -5,27 +5,31 @@ export default async function handler(req, res) {
     const { email } = req.body;
 
     try {
-  const response = await fetch(`https://us6.api.mailchimp.com/3.0/lists/${process.env.NEXT_PUBLIC_MAILCHIMP_LIST_ID}/members`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Basic ${Buffer.from(`anystring:${process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY}`).toString('base64')}`
-    },
-    body: JSON.stringify({
-      email_address: email,
-      status: 'subscribed'
-    })
-  });
+      const response = await fetch(`https://us6.api.mailchimp.com/3.0/lists/${process.env.NEXT_PUBLIC_MAILCHIMP_LIST_ID}/members`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Basic ${Buffer.from(`anystring:${process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY}`).toString('base64')}`
+        },
+        body: JSON.stringify({
+          email_address: email,
+          status: 'subscribed'
+        })
+      });
 
-  const responseData = await response.json();
+      const responseData = await response.json();
 
-  if (response.ok) {
-    res.status(200).json({ message: 'Thanks for subscribing!' });
+      if (response.ok) {
+        res.status(200).json({ message: 'Thanks for subscribing!' });
+      } else {
+        console.error('Mailchimp API error:', responseData);
+        res.status(500).json({ message: 'Sorry, something went wrong. Please try again later.' });
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ message: 'Sorry, something went wrong. Please try again later.' });
+    }
   } else {
-    console.error('Mailchimp API error:', responseData);
-    res.status(500).json({ message: 'Sorry, something went wrong. Please try again later.' });
+    res.status(405).json({ message: 'Method not allowed' });
   }
-} catch (error) {
-  console.error('Error:', error);
-  res.status(500).json({ message: 'Sorry, something went wrong. Please try again later.' });
 }
