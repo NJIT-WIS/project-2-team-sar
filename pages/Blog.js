2a25c3f6bf3bf85b9bfb020a6f52f997ac46511d
rlantigua01 <123276441+rlantigua01@users.noreply.github.com>
2023-05-02 00:27:55 -0400

    Create Blog.js

---
+++ pages/Blog.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+
+export default function Blog({ allPostsData }) {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <Header />
+      <main>
+        {allPostsData.map(({ title, excerpt, keyword }) => (
+          <div key={keyword}>
+            <Link href={`/posts/${keyword}`}>
+              <a>
+                <h2>{title}</h2>
+              </a>
+            </Link>
+            <p>{excerpt}</p>
+          </div>
+        ))}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
