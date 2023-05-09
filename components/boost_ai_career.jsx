0feacbae885740455aac23196948552f56c508fa
rjd <rl474@njit.edu>
2023-05-08 20:41:22 -0400

    updated meta name

--- components/boost_ai_career.jsx
+++ components/boost_ai_career.jsx
@@ -13,7 +13,7 @@ const Services = () => {
         <Col md={4}>
           <Card>
             <Card.Body>
-              <Card.Title>Professional Development</Card.Title>
+              <Card.Title>Professional Career Development</Card.Title>
               <ListGroup variant="flush">
                 <ListGroup.Item>Certification Programs: AI Engineer, Data Scientist, and ML Specialist</ListGroup.Item>
                 <ListGroup.Item>Industry Partnerships: Collaborate with top AI companies for hands-on experience</ListGroup.Item>
