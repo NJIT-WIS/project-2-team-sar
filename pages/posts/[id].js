1cdce470d14092aeb6114239fc147c713c890b99
rlantigua01 <123276441+rlantigua01@users.noreply.github.com>
2023-05-09 22:27:45 -0400

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -21,6 +21,23 @@ export default function Post({ postData }) {
 }
 
 export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
   const ids = getAllPostIds();
   const paths = ids.map((id) => ({
     params: { id },
