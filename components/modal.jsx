821d5ace1b24e1e9ee6fa18c9999a695a705e75e
Swapnil62 <sp2428@njit.edu>
2023-05-09 20:29:55 -0400

    Added link to "privacy" page in cookie-modal

--- components/modal.jsx
+++ components/modal.jsx
@@ -105,7 +105,10 @@ export default function CookieConsentModal() {
           <div className="modal-content">
             <h2>Cookie Consent</h2>
             <p>
-              We use cookies to enhance your experience on our website. By continuing to use our website, you consent to our use of cookies.
+            We use cookies to personalize content and ads, to provide social media features and to analyze our traffic. We also share information about your use of our site with our social media, advertising and analytics partners who may combine it with other information that you’ve provided to them or that they’ve collected from your use of their services.
+            </p>
+            <p>
+            By clicking "Accept", you consent to the use of all cookies and the transfer of information to Google Analytics. You can review our <a href="/privacy">Privacy Policy</a> for more information about how we collect and use data.
             </p>
             <div className="modal-buttons">
               <button onClick={handleAccept}>Accept</button>
@@ -121,7 +124,6 @@ export default function CookieConsentModal() {
           left: 0;
           width: 100%;
           height: 100%;
-          background-color: rgba(0, 0, 0, 0.5);
           display: flex;
           justify-content: center;
           align-items: center;
@@ -131,10 +133,18 @@ export default function CookieConsentModal() {
           background-color: white;
           padding: 20px;
           border-radius: 5px;
+          width: 450px;
+          height: 500px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
         }
         .modal-buttons {
           display: flex;
           justify-content: space-between;
+          width: 100%;
           margin-top: 20px;
         }
       `}</style>
